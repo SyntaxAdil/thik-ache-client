63eaa5b27397f12c490c6f2faf8621956ce1bc65
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-07-12 17:37:59 +0600

    Update .gitignore to include .vscode and remove README.md; modify next.config.ts for type definition

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
 
-const nextConfig: NextConfig = {
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
