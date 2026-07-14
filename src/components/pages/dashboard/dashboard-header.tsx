748f9ddd9b4caa1b8b96e8f0f65c91949b1128a3
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-07-14 22:40:18 +0600

    feat: Refactor DashboardHeader to improve title handling and add display logic

--- src/components/pages/dashboard/dashboard-header.tsx
+++ src/components/pages/dashboard/dashboard-header.tsx
@@ -15,22 +15,27 @@ import AvatarUserDropdown from "../../shared/avatar-dropdown";
 import DashboardNotification from "./dashboard-notification";
 
 interface DashboardHeaderProps {
-  title: string;
+  title?: string;
 }
 
 export default function DashboardHeader({
   title,
 }: DashboardHeaderProps): React.JSX.Element {
   const pathname = usePathname();
-
   const segments = pathname ? pathname.split("/").filter(Boolean) : [];
+  
+  const lastSegment = segments[segments.length - 1];
+  const formattedName = lastSegment
+    ? lastSegment.charAt(0).toUpperCase() + lastSegment.slice(1)
+    : "";
+
+  const displayTitle = formattedName || title || "Dashboard";
 
   return (
     <nav className="flex items-center justify-between gap-4 pe-4 py-2 border-b border-zinc-900 mb-4 select-none sticky top-0 left-0 right-0 z-50 bg-zinc-950">
-      {/* Header Title */}
       <div className="flex items-center gap-6">
-        <h1 className=" hidden md:block text-2xl font-bold tracking-tight text-white">
-          {title || "Dashboard"}
+        <h1 className="hidden md:block text-2xl font-bold tracking-tight text-white">
+          {displayTitle}
         </h1>
 
         <Breadcrumb>
@@ -42,7 +47,7 @@ export default function DashboardHeader({
             {segments.map((segment, index) => {
               const href = `/${segments.slice(0, index + 1).join("/")}`;
               const isLast = index === segments.length - 1;
-              const formattedName =
+              const formattedSegment =
                 segment.charAt(0).toUpperCase() + segment.slice(1);
 
               return (
@@ -50,10 +55,10 @@ export default function DashboardHeader({
                   <BreadcrumbSeparator />
                   <BreadcrumbItem>
                     {isLast ? (
-                      <BreadcrumbPage>{formattedName}</BreadcrumbPage>
+                      <BreadcrumbPage>{formattedSegment}</BreadcrumbPage>
                     ) : (
                       <BreadcrumbLink render={<Link href={href} />}>
-                        {formattedName}
+                        {formattedSegment}
                       </BreadcrumbLink>
                     )}
                   </BreadcrumbItem>
@@ -63,7 +68,7 @@ export default function DashboardHeader({
           </BreadcrumbList>
         </Breadcrumb>
       </div>
-      {/* Notification & Avatar */}
+
       <div className="flex items-center gap-3">
         <DashboardNotification />
         <AvatarUserDropdown variant="dashboard" />
